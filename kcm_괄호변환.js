bf63dd3afaab9b9ec744cea79dc3b72f1921a945
chmini <beelzebule@naver.com>
2021-11-05 17:11:58 +0900

    김창민 괄호변환

---
+++ kcm_괄호변환.js
@@ -0,0 +1,42 @@
+const separate = (str) => {
+    let open = 0;
+    let close = 0;
+
+    for (let letter of str) {
+        letter === "(" ? open++ : close++;
+        if (open === close)
+            return [str.slice(0, close * 2), str.slice(close * 2)];
+    }
+};
+
+const isCorrect = (str) => {
+    /*
+        const stack = [];
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === "(") {
+                stack.push("(");
+            } else {
+                if (!stack.pop()) return false;
+            }
+        }
+        return true;
+    */
+    return str[0] === "(" && str[str.length - 1] === ")";
+};
+
+const makeCorrect = (str) => {
+    return str
+        .split("")
+        .slice(1, str.length - 1)
+        .map((letter) => (letter === "(" ? ")" : "("))
+        .join("");
+};
+
+const solution = (str) => {
+    if (!str) return str;
+
+    const [u, v] = separate(str);
+
+    if (isCorrect(u)) return u + solution(v);
+    else return "(" + solution(v) + ")" + makeCorrect(u);
+};
