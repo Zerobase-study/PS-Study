4fcb61a7db9c29a981aa5210cc638628695b261c
ULemong <maintain9912@gmail.com>
2022-01-06 13:48:48 +0900

    #57 미로탐색 유지현

--- 유지현/220104미로탐색.js
+++ 유지현/220104미로탐색.js
@@ -1,45 +1,36 @@
-const N = 7;
-const M = 7;
-const input = "1011111 1110001 1000001 1000001 1000001 1000001 1111111";
+function findmaze(row, col, input) {
+  const graph = [];
 
-const graph = [];
-
-const data = input.split(" ");
-for (let i = 0; i < N; i++) {
-  graph[i] = data[i].split("").map(Number);
-}
-
-function BFS(x, y) {
-  const dx = [-1, 1, 0, 0];
-  const dy = [0, 0, -1, 1];
-
-  const queue = [];
-  queue.push({ x, y });
-
-  while (queue.length) {
-    const { x, y } = queue.shift();
-
-    for (let i = 0; i < 4; i++) {
-      const nx = x + dx[i];
-      const ny = y + dy[i];
-
-      if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-
-      if (graph[nx][ny] === 1) {
-        graph[nx][ny] = graph[x][y] + 1;
-        queue.push({ x: nx, y: ny });
-      }
-
-      // if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-      //   if (graph[nx][ny] === 1) {
-      //     graph[nx][ny] = graph[x][y] + 1;
-      //     queue.push({ x: nx, y: ny });
-      //   }
-      // }
-    }
+  const data = input.split(" ");
+  for (let i = 0; i < row; i++) {
+    graph[i] = data[i].split("").map(Number);
   }
 
-  return graph[N - 1][M - 1];
+  function BFS(x, y) {
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
+
+    const queue = [];
+    queue.push({ x, y });
+
+    while (queue.length) {
+      const { x, y } = queue.shift();
+
+      for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
+
+        if (nx >= 0 && ny >= 0 && nx < row && ny < col && graph[nx][ny] === 1) {
+          graph[nx][ny] = graph[x][y] + 1;
+          queue.push({ x: nx, y: ny });
+        }
+      }
+    }
+    return graph[row - 1][col - 1];
+  }
+  return BFS(0, 0);
 }
 
-console.log(BFS(0, 0));
+console.log(
+  findmaze(7, 7, "1011111 1110001 1000001 1000001 1000001 1000001 1111111")
+);
