f9208675c61e8ba7adc58f377949f1c7f61ba9e1
itsyuna <ingyouna@naver.com>
2022-03-15 15:56:00 +0900

    #102 탐욕법

---
+++ 이유나/구명보트.js
@@ -0,0 +1,24 @@
+function solution(people, limit) {
+  const ascedingOrder = people.sort((a, b) => a - b);
+  let count = 0;
+
+  while (ascedingOrder.length > 0) {
+    for (let i = ascedingOrder.length - 1; i >= 0; i--) {
+      let j = 0;
+      if (ascedingOrder[i] + ascedingOrder[j] <= limit) {
+        ascedingOrder.pop(ascedingOrder[i]);
+        ascedingOrder.shift(ascedingOrder[j]);
+        j++;
+        i--;
+      } else {
+        ascedingOrder.pop(ascedingOrder[i]);
+      }
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(solution([70, 50, 80, 50], 100)); // 3
+console.log(solution([70, 80, 50], 100)); // 3
